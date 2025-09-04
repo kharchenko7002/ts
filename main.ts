6f550b0f16f67a67f52093782a8d048368ab8399
kharchenko7002 <kharchenko7002@gmail.com>
2025-09-04 12:33:34 +0200

    variabler

--- main.ts
+++ main.ts
@@ -4,3 +4,10 @@ const firtsName: string = 'Oskar';
 let helloMessage: string = `Hello, ${firtsName}`;
 
 console.log(helloMessage);
+
+const sant: boolean=true;
+const usant: boolean=false;
+
+
+const empty:null = null;
+const undefinedName:undefined = 
